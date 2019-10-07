fb1429a850c4197a90659e18b5619b54830cea7f
Jose Manuel Moreno Gavira <josemanuel.moreno@dominion-global.com>
2019-10-07 13:06:31 +0200

    one line sql files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,3 @@
-insert into books (id, isbn, title, author)
-values (1, '0134602412', 'Microservices: Flexible Software Architecture', 'Eberhard Wolff');
-
-insert into books (id, isbn, title, author)
-values (2, '1484231643', 'Learn Microservices with Spring Boot', 'Moises Macero');
-
-insert into books (id, isbn, title, author)
-values (3, 'B072KJXXZD', 'Advanced Microservices', 'Hunter II, Thomas');
+insert into books (id, isbn, title, author) values (1, '0134602412', 'Microservices: Flexible Software Architecture', 'Eberhard Wolff');
+insert into books (id, isbn, title, author) values (2, '1484231643', 'Learn Microservices with Spring Boot', 'Moises Macero');
+insert into books (id, isbn, title, author) values (3, 'B072KJXXZD', 'Advanced Microservices', 'Hunter II, Thomas');
