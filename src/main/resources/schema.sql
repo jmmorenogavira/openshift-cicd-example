afbf3d5c500bdcdf17cf68095224f7d3544fc9ec
Jose Manuel Moreno Gavira <josemanuel.moreno@dominion-global.com>
2019-10-07 08:34:49 +0200

    Added database schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (
+    id   BIGSERIAL NOT NULL AUTO_INCREMENT,
+    isbn VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
